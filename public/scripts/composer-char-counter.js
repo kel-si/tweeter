a9751451346f1e76de19c161fae2e0c56530eb9f
Kelsi Olstad <kelsiolstad@gmail.com>
2022-03-02 16:27:53 -0700

    fix counter to change color and position correctly

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,15 +2,16 @@ $(document).ready(function () {
   console.log("DOM has loaded");
 
   $('#tweet-text').on('input', function(event) {
+    console.log("this", this)
     let charCount = $(this).val().length;
     let currentCount = $(this).siblings('div').children('.counter').text(140 - charCount);
     console.log("currentCount", currentCount);
 
     if (charCount > 140) {
-      currentCount.addClass('over-count')
-    }
-    if (charCount <= 140) {
-      currentCount.removeClass('over-count')
+      $(".counter").addClass('over-count')
+    } else {
+      $(".counter").removeClass('over-count')
+    
     }
 
   });
