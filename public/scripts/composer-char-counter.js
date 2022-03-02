615e933c1e21e8ee04cd0cc72a1e5ced29c38aa8
Kelsi Olstad <kelsiolstad@gmail.com>
2022-03-01 20:21:34 -0700

    counter change colour when over character limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,13 +4,10 @@ $(document).ready(function () {
   $('#tweet-text').on('input', function(event) {
     let charCount = $(this).val().length;
     let currentCount = $(this).siblings('div').children('.counter').text(140 - charCount);
-    
-  })
+    console.log("currentCount", currentCount);
 
+    if (charCount > 140) {
+      currentCount.addClass('over-count')
+    }
+  });
 });
-
-
-
-  // console.log("this.length", $(this).val().length); //length of the input (minus one because it starts at zero)
-  // console.log("this", this) //line of html triggering the event
-  // console.log("charCounter", charCount);
