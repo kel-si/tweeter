8c75e55d1ebf68d003b73901dbaa495d562976b0
Kelsi Olstad <kelsiolstad@gmail.com>
2022-03-02 21:08:47 -0700

    finish dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,13 +3,56 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
 
 $(document).ready(function () {
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      const $returnValue = createTweetElement(tweet);
+      $('.tweet-container').append($returnValue);
+    }
+  }
 
   //adds an article with the tweet info to the DOM
   const createTweetElement = function(tweetObj) {
     const date = new Date(tweetObj.created_at);
-    const $tweet = $(`<article class="tweet">
+    const $tweet = `<article class="tweet">
     <header>
       <div class="tweet-user">
       <img src=${tweetObj.user.avatars}>
@@ -26,34 +69,16 @@ $(document).ready(function () {
       <i class="fa-solid fa-heart"></i>
     </div>
     </footer>
-  </article>`);  
+  </article>`;  
   return $tweet;
   }
   
   const $tweet = createTweetElement(tweetData);
   $('.tweet-container').append($tweet);
-  
-  //takes an array of tweet objects
-  const renderTweets = function(tweets) {
-    for(let tweet in tweets) {
-      createTweetElement(tweet);
-      // $('.timeago').append(timeago.format(new Date()));
-    }
-    
+  console.log("$tweet", $tweet)
 
-  }
-
+  renderTweets(data);
 });
 
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227,
-}
+
