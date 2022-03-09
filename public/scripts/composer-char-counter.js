26b29efb3cf3b8167bbfb07667cdabe5597bff3c
Kelsi Olstad <kelsiolstad@gmail.com>
2022-03-08 20:57:15 -0700

    remove console.log

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-  console.log("DOM has loaded");
-
   $('#tweet-text').on('input', function(event) {
     let charCount = $(this).val().length;
     let currentCount = $(this).siblings('div').children('.counter').text(140 - charCount);
